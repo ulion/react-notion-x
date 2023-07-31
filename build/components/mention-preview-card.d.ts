3568a914237f274c889aa7d51fe5d04e5875b917
Ulion <ulion2002@gmail.com>
2023-07-31 23:09:07 +0800

    gitpkg

---
+++ build/components/mention-preview-card.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+export declare const MentionPreviewCard: React.FC<{
+    owner?: string;
+    lastUpdated?: string;
+    title: string;
+    domain: string;
+    externalImage?: React.ReactNode;
+}>;
+//# sourceMappingURL=mention-preview-card.d.ts.map
